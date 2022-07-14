bef62f02885f60009a5135e3dafc2d5517ec151d
Pooja-Desh <100403024+Pooja-Desh@users.noreply.github.com>
2022-07-15 03:01:13 +0530

    Update misaligned.cpp

--- misaligned.cpp
+++ misaligned.cpp
@@ -1,24 +1,23 @@
 #include <iostream>
 #include <assert.h>
 
-struct colorPairList
-    {
-        int colorPairID;
-        char* majorColorName;
-        char* minorColorName;
-    };
-colorPairList colorPair[25] ={};
+const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
+const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+    
+int numberOfMajorColor = sizeof(majorColor)/sizeof(majorColor[0]);
+int numberOfMinorColor = sizeof(minorColor)/sizeof(minorColor[0]);
+int maxNumberofColorPairs = numberOfMajorColor * numberOfMinorColor;
+    
+int lengthOflongestMajorColor = 0;
+int formatCounterStub = 0;
+int printCounterStub = 0;
 
 int printColorMap() {
-    const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
-    const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
     
     int i = 0, j = 0;
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
             std::cout << i * 5 + j << " | " << majorColor[i] << " | " << minorColor[i] << "\n";
-            int colorPairID = i * 5 + j ;
-            colorPair[colorPairID] = {colorPairID,majorColor[i],majorColor[i]};
         }
     }
     return i * j;
@@ -28,13 +27,6 @@ int main() {
     int result = printColorMap();
     assert(result == 25);
     std::cout << "All is well (maybe!)\n";
-    
-    for(int i=0;i<5;i++){
-        for(int j=0;j<5;j++){
-            colorPairID = i * 5 + j;
-            assert(colorPair[colorPairID].minorColorName == minorColor[j]);
-        }
-    }
             
     return 0;
 }
