053b20d93b658f423a9bfbd08a4394c57bd29217
Pooja-Desh <100403024+Pooja-Desh@users.noreply.github.com>
2022-08-19 18:37:34 +0530

    Update misaligned.cpp

--- misaligned.cpp
+++ misaligned.cpp
@@ -12,19 +12,42 @@ int lengthOflongestMajorColor = 0;
 int formatCounterStub = 0;
 int printCounterStub = 0;
 
-int printColorMap() {
-    
-    int i = 0, j = 0;
-    for(i = 0; i < 5; i++) {
-        for(j = 0; j < 5; j++) {
-            std::cout << i * 5 + j << " | " << majorColor[i] << " | " << minorColor[i] << "\n";
-        }
-    }
-    return i * j;
+std::vector<std::string> colorMap[maxNumberofColorPairs] = {""};
+
+std::string getColorPair(int majorColorNumber,int minorColorNumber)
+{
+    std::string colorPairString = "";
+    return colorPairString.append(majorColor[majorColorNumber]).append("|").append(minorColor[minorColorNumber]);
 }
 
-int main() {
-    int result = printColorMap();
+std::vector<std::string> generateColorPairMap(std::vector<std::string> colorMap)
+{
+    //std::string colorMap[maxNumberofColorPairs] = {""};
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            std::string receivedColorPair = getColorPair(i,j);
+            colorMap[(i * 5 + j)] = receivedColorPair;
+        }
+    }
+    return(colorMap);
+}
+
+int printColorMap(std::vector<std::string> colorMap)
+{   
+    std::vector<std::string> generatedColorMap = generateColorPairMap(colorMap);
+    for(int i = 0; i < maxNumberofColorPairs; i++) 
+    {
+        cout<<(i+1)<<" | "<< generatedColorMap[i]<<"\n";
+    }
+    return i;
+}
+
+int main() 
+{
+    std::vector<std::string> testColorMap;
+    int result = printColorMap(testColorMap);
     assert(result == 25);
     std::cout << "All is well (maybe!)\n";
             
